c9746ac75a70d4d4975ff6907f44d948bd824825
Abdullah ` <ywtywbbdallh424@gmail.com>
2026-08-03 00:50:10 +0530

    icon added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
+import tabIcon from "@/assets/tab.png";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 function NotFoundComponent() {
@@ -91,7 +92,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: tabIcon, type: "image/png" },
+      { rel: "apple-touch-icon", href: tabIcon },
     ],
   }),
   shellComponent: RootShell,
